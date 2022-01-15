45da19d6eb851eacd18a5340a241cc9b2d68f594
Rad-hi <radhisghaier33@gmail.com>
2022-01-15 18:39:22 +0100

    Created a shred configs file for shared config constants

---
+++ src/Configs.h
@@ -0,0 +1,26 @@
+/* 
+ * -----------------------------------------------------------------------------
+ * Library: MyButton & MyCountingButton
+ * This file defines shared constants between the two files
+ * -----------------------------------------------------------------------------
+ * Author: Radhi SGHAIER: https://github.com/Rad-hi
+ * -----------------------------------------------------------------------------
+ * Date: 15-01-2022 (15th of January, 2022)
+ * -----------------------------------------------------------------------------
+ * License: Do whatever you want with the code ...
+ *          If this was ever useful to you, and we happened to meet on 
+ *          the street, I'll appreciate a cup of dark coffee, no sugar please.
+ * -----------------------------------------------------------------------------
+ */
+
+#ifndef __CONFIGS_H__
+  #define __CONFIGS_H__
+
+  #include "Arduino.h"
+
+  #define NORMAL_UP                      1     /* Normally closed or open */
+  #define NORMAL_DOWN                    0
+
+  #define DEFAULT_DEBOUNCE               5     /* Milliseconds */
+
+#endif // __CONFIGS_H__ 
